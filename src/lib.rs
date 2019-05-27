f535994aa4efdf84c9bb670f17726b19ef987231
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-05-27 16:10:50 +0200

    v0.2.1
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/tcp-stream/0.2.0/")]
+#![doc(html_root_url = "https://docs.rs/tcp-stream/0.2.1/")]
 
 //! # Improve mio's TCP stream handling
 //!
