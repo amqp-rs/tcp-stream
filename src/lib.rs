70afbd0fdf6aff31a9b8f583eced791f504be285
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-22 16:24:56 +0200

    merge files
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -592,7 +592,51 @@ impl From<io::Error> for HandshakeError {
 }
 
 #[cfg(unix)]
-mod unix;
+mod sys {
+    use crate::TcpStream;
+    use mio::net::TcpStream as MioTcpStream;
+    use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
+
+    impl AsRawFd for TcpStream {
+        fn as_raw_fd(&self) -> RawFd {
+            <MioTcpStream as AsRawFd>::as_raw_fd(self)
+        }
+    }
+
+    impl AsRawFd for &TcpStream {
+        fn as_raw_fd(&self) -> RawFd {
+            <MioTcpStream as AsRawFd>::as_raw_fd(self)
+        }
+    }
+
+    impl FromRawFd for TcpStream {
+        unsafe fn from_raw_fd(fd: RawFd) -> Self {
+            MioTcpStream::from_raw_fd(fd).into()
+        }
+    }
+}
 
 #[cfg(windows)]
-mod windows;
+mod sys {
+    use crate::TcpStream;
+    use mio::net::TcpStream as MioTcpStream;
+    use std::os::windows::io::{AsRawSocket, FromRawSocket, RawSocket};
+
+    impl AsRawSocket for TcpStream {
+        fn as_raw_socket(&self) -> RawSocket {
+            <MioTcpStream as AsRawSocket>::as_raw_socket(self)
+        }
+    }
+
+    impl AsRawSocket for &TcpStream {
+        fn as_raw_socket(&self) -> RawSocket {
+            <MioTcpStream as AsRawSocket>::as_raw_socket(self)
+        }
+    }
+
+    impl FromRawSocket for TcpStream {
+        unsafe fn from_raw_socket(socket: RawSocket) -> Self {
+            MioTcpStream::from_raw_socket(socket).into()
+        }
+    }
+}
