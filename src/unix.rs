05ff40fe99aad9300aed933d7bb4ff1369d30062
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-22 09:37:52 +0200

    impl AsRawFd/Socket for references too
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/unix.rs
+++ src/unix.rs
@@ -10,6 +10,12 @@ impl AsRawFd for TcpStream {
     }
 }
 
+impl AsRawFd for &TcpStream {
+    fn as_raw_fd(&self) -> RawFd {
+        <MioTcpStream as AsRawFd>::as_raw_fd(self)
+    }
+}
+
 impl FromRawFd for TcpStream {
     unsafe fn from_raw_fd(fd: RawFd) -> Self {
         MioTcpStream::from_raw_fd(fd).into()
--- src/windows.rs
+++ src/windows.rs
@@ -10,6 +10,12 @@ impl AsRawSocket for TcpStream {
     }
 }
 
+impl AsRawSocket for &TcpStream {
+    fn as_raw_socket(&self) -> RawSocket {
+        <MioTcpStream as AsRawSocket>::as_raw_socket(self)
+    }
+}
+
 impl FromRawSocket for TcpStream {
     unsafe fn from_raw_socket(socket: RawSocket) -> Self {
         MioTcpStream::from_raw_socket(socket).into()
