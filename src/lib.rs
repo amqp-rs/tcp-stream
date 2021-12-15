8d01668e354bd6387461fa6c99a78dddedbbabd8
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2021-12-15 10:44:48 +0100

    v0.20.9
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/tcp-stream/0.20.8/")]
+#![doc(html_root_url = "https://docs.rs/tcp-stream/0.20.9/")]
 
 //! # mio's TCP stream on steroids
 //!
