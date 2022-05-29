39772068974fb37a9dcd35cde2b26cad6cc6c3ae
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2022-05-29 10:54:26 +0200

    rustfmt
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -300,7 +300,10 @@ fn connect_std<A: ToSocketAddrs>(addr: A, timeout: Option<Duration>) -> io::Resu
     Ok(stream)
 }
 
-fn connect_std_raw<A: ToSocketAddrs>(addr: A, timeout: Option<Duration>) -> io::Result<StdTcpStream> {
+fn connect_std_raw<A: ToSocketAddrs>(
+    addr: A,
+    timeout: Option<Duration>,
+) -> io::Result<StdTcpStream> {
     let mut addrs = addr.to_socket_addrs()?;
     let mut err = None;
     if let Some(timeout) = timeout {
