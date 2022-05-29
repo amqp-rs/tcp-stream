de124b3f35ba0d0ebc76475944a47b1f6b5a1eb6
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2022-05-29 10:36:18 +0200

    v0.24.4
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/tcp-stream/0.24.3/")]
+#![doc(html_root_url = "https://docs.rs/tcp-stream/0.24.4/")]
 
 //! # std::net::TCP stream on steroids
 //!
